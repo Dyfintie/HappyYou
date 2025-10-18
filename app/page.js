848efe093adf9b7327fc1aed77e7a26e0104dff0
Dyfintie <v08pandey@gmail.com>
2025-10-18 12:24:31 +0530

    new update

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Navbar from "./components/NavbarComp/Navbar";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/HeroComp/Hero";
 const Home = () => {
   return (
     <div>
@@ -14,8 +16,11 @@ const Home = () => {
        */}
       <Navbar />
       {/* After the navbar here there will be the main body the cover or the hero  */}
+      <Hero />
       {/* After the hero there will be a extended hero of info section  */}
+
       {/* Finally the footer section  */}
+      <Footer />
     </div>
   );
 };
